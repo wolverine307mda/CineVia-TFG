6033712def72a4f88861dc8762e0eba1fed8f231
Mario de Domingo <wolverine.mda.307@gmail.com>
2025-04-13 20:06:52 +0200

    añadiendo el guid generator

---
+++ backend/src/main/kotlin/org/wolve/geofilm/utils/GuidGenerator.kt
@@ -0,0 +1,18 @@
+package org.wolve.geofilm.utils
+
+import kotlin.random.Random
+
+class GuidGenerator {
+
+    fun generarId(): String {
+        val caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        val id = StringBuilder()
+
+        repeat(11) {
+            val index = Random.nextInt(0, caracteres.length)
+            id.append(caracteres[index])
+        }
+
+        return id.toString()
+    }
+}
